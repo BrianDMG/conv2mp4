8908464a934d8557cfa9cd7d20e24430d3db8c50
BrianDMG <github@teague.io>
2017-10-24 22:03:24 -0400

    Trimmed some fat
    * Removed unnecessary/redundant rounding on $diff* variables
    * Removed unnecessary whitespace
    
    Bring up to date with master (#28)
    
    * Added Subfolder retention capability
    
    * Keep subfolder structure when saving
    
    Fixed a bug that I found in my last change
    
    Semantic changes
    
    * Changed variable name for uniformity
    * Trimmed more unnecessary whitespace
    
    Contributions
    
    * Added pull requests message
    
    $outPath note
    
    * Added additional information in variable description to indicate that
    the directory structure is preserved when using $outPath
    
    Added link to contributing guidelines
    
    Created
    
    Initial commit
    
    Tidy up
    
    Test
    
    Untest
    
    Removed guidelines doc for staging
    
    Delete README.md

--- cfg_conv2mp4-ps.ps1
+++ cfg_conv2mp4-ps.ps1
@@ -20,14 +20,16 @@ $logName = the filename of the log file
 $usePlex = If set to $True, Plex settings will be used. Set to $False if Plex feature is not needed
 $plexIP = the IP address and port of your Plex server (for the purpose of refreshing its libraries)
 $plexToken = your Plex server's token (for the purpose of refreshing its libraries).
-NOTE: Plex server token - See https://support.plex.tv/hc/en-us/articles/204059436-Finding-your-account-token-X-Plex-Token. Plex server token is also easy to retrieve with PlexPy, Ombi, Couchpotato, or SickRage.
+	NOTE: Plex server token - See https://support.plex.tv/hc/en-us/articles/204059436-Finding-your-account-token-X-Plex-Token.
+	Plex server token is also easy to retrieve with PlexPy, Ombi, Couchpotato, or SickRage.
 $ffmpegBinDir = path to ffmpeg bin folder (no trailing "\"). This is the directory containing ffmpeg.exe and ffprobe.exe 
 $handbrakeDir = path to Handbrake directory (no trailing "\"). This is the directory containing HandBrakeCLI.exe
 collectGarbage = $True enables garbage collection. $False disables garbage collection.
 $script:garbage = the extensions of the files you want garbage collection to delete in the format "*.ex1", "*.ex2"
 $appendLog = $False will clear the log at the beginning of every session, $True will append new session log to old session log.
 $keepSubs = $False will discard subtitles from converted files. $True will keep subtitles.
-$useOutPath = $False will use $mediaPath as the output folder. $True will output converted files to $outPath
+$useOutPath = $False will use $mediaPath as the output folder. $True will output converted files to $outPath.
+	NOTE: Directory structure is preserved in $outPath
 $outPath = If $useOutPath = $True, converted files will be written to this directory (no trailing "\")
 -----------------------------------------------------------------------------------------------------------------------#>
 $mediaPath = "\\your\path\here"
