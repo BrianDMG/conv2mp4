a4d91b7160727e3aab107afe7da084f088b5a74b
djgrijalva <djgrijalva@users.noreply.github.com>
2017-11-29 16:33:02 -0600

    Update conv2mp4-ps.ps1

--- conv2mp4-ps.ps1
+++ conv2mp4-ps.ps1
@@ -3,7 +3,7 @@ conv2mp4-ps v4.0 RELEASE - https://github.com/BrianDMG/conv2mp4-ps
 
 This Powershell script will recursively search through a user-defined file path and convert all videos of user-specified 
 filetypes to MP4 with H264 video and AAC audio using ffmpeg. If a conversion failure is detected, the script re-encodes
-the file with HandbrakeCLI. Upon successful encoding, Plex libraries are refreshed and source file is deleted. 
+the file with HandbrakeCLI. Upon successful encoding, Plex libraries are (optionally) refreshed and source file is deleted. 
 The purpose of this script is to reduce the amount of transcoding CPU load on a Plex server.
 ========================================================================================================================
 
