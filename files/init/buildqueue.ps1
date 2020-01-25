62dd39fc894a37d5f049f4f29af5d188d4138dd4
BrianDMG <github@teague.io>
2020-01-24 19:49:24 -0500

    Improved formatting for fileList

--- files/init/buildqueue.ps1
+++ files/init/buildqueue.ps1
@@ -5,6 +5,11 @@ Write-Output "`nBuilding file list, please wait. This may take a while, especial
 $fileCount=0
 $mPath = Get-Item -Path $cfg.media_path
 $ignoreList = Get-Content $prop.ignore_path
-$fileList = Get-ChildItem "$($mPath.FullName)" -Include ( $cfg.include_file_types -split ',' ).trim() -Exclude $ignoreList -Recurse | ForEach-Object { $fileCount++; If ($fileCount -eq 1) { Write-Progress "`rFound $fileCount file so far..." } Else { Write-Progress "`rFound $fileCount files so far..." };$_}
+$fileList = Get-ChildItem "$($mPath.FullName)" -Include ( $cfg.include_file_types -split ',' ).trim() -Exclude $ignoreList -Recurse |
+    ForEach-Object {
+        $fileCount++
+        Write-Progress "`rFound $fileCount file(s) so far..."
+        $_
+    }
 
 PrintFileQueue
