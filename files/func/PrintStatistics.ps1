d904e442ef7d7b04597e882557429de008924b4b
BrianDMG <github@teague.io>
2020-01-31 17:40:16 -0500

    Removed unnecessary '$script:' prefixes

--- files/func/PrintStatistics.ps1
+++ files/func/PrintStatistics.ps1
@@ -1,21 +1,21 @@
 # Log various session statistics
 Function PrintStatistics {
-    If ($fileList.Count -ge 1) {
+    If ($fileList.Count -ge 1 -AND $cumulativeVideoDuration -ne "00:00:00") {
         Log "`n$($prop.final_stat_divider)`n"
         #Print total session disk usage changes
-        If ($script:diskUsageDelta -gt -1 -AND $script:diskUsageDelta -lt 1) {
-            $diskUsageDelta_KB = ($script:diskUsageDelta * 1024)
+        If ($diskUsageDelta -gt -1 -AND $diskUsageDelta -lt 1) {
+            $diskUsageDelta_KB = ($diskUsageDelta * 1024)
             $diskUsageDelta_KB = [Math]::Round($diskUsageDelta_KB, 2)
             Log "$($time.Invoke()) Total cumulative storage difference: $($diskUsageDelta_KB)KB"
         }
-        Elseif ($script:diskUsageDelta -lt -1024 -OR $script:diskUsageDelta -gt 1024) {
-            $diskUsageDelta_GB = ($script:diskUsageDelta / 1024)
+        Elseif ($diskUsageDelta -lt -1024 -OR $diskUsageDelta -gt 1024) {
+            $diskUsageDelta_GB = ($diskUsageDelta / 1024)
             $diskUsageDelta_GB = [Math]::Round($diskUsageDelta_GB, 2)
             Log "$($time.Invoke()) Total cumulative storage difference: $($diskUsageDelta_GB)GB"
         }
         Else {
-            $script:diskUsageDelta = [Math]::Round($script:diskUsageDelta, 2)
-            Log "$($time.Invoke()) Total cumulative storage difference: $($script:diskUsageDelta)MB"
+            $diskUsageDelta = [Math]::Round($diskUsageDelta, 2)
+            Log "$($time.Invoke()) Total cumulative storage difference: $($diskUsageDelta)MB"
         }
 
         #Do some time math to get total script runtime
@@ -24,11 +24,11 @@ Function PrintStatistics {
         $scriptExecutionDurationFormat = $scriptExecutionDuration.ToString()
         $scriptExecutionDurationFormat = $scriptExecutionDurationFormat.Substring(0, $scriptExecutionDurationFormat.IndexOf('.'))
         
-        Log "`n$script:cumulativeVideoDuration of video processed in $scriptExecutionDurationFormat"
+        Log "`n$cumulativeVideoDuration of video processed in $scriptExecutionDurationFormat"
 
         #Do some math/rounding to get session average conversion speed
         Try {
-            $averageConversionRate = $script:cumulativeVideoDuration.Ticks / $scriptExecutionDuration.Ticks
+            $averageConversionRate = $cumulativeVideoDuration.Ticks / $scriptExecutionDuration.Ticks
             $averageConversionRate = [math]::Round($averageConversionRate, 2)
             Log "Average conversion speed of $($averageConversionRate)x"
         }
@@ -36,7 +36,9 @@ Function PrintStatistics {
             Log "$_"
             Log "No time elapsed."
         }
-
-        Log "`n$($prop.final_stat_divider)"
     }
+    Else {
+        Write-Output "`nNo video was encoded/converted."
+    }
+    Log "`n$($prop.final_stat_divider)"
 }
