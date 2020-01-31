2e3f4accbd6a0af99ad3d9ba05b032c558605e8b
BrianDMG <github@teague.io>
2020-01-31 16:54:54 -0500

    Simplified count code

--- files/func/CollectGarbage.ps1
+++ files/func/CollectGarbage.ps1
@@ -2,28 +2,16 @@
 Function CollectGarbage {
 
     $garbageList = Get-ChildItem "$((Get-Item -Path $cfg.media_path).FullName)" -Include ( $cfg.garbage_include_file_types -split ',' ).trim() -Recurse
-    $garbageNum = 0
 
-    ForEach ($turd in $garbageList) {
-            $garbageNum++
-    }
-
-    If ($garbageNum -eq 1) {
-        Log "`nGarbage Collection: The following file was deleted:"
-    }
-    Elseif ($garbageNum -gt 1) {
-        Log "`nGarbage Collection: The following $garbageNum files were deleted:"
+    If ($garbageList.Count -ge 1) {
+        Log "`nGarbage Collection: The following $($garbageList.Count) file(s) were deleted:"
     }
     Else {
         Write-Output "Garbage Collection: No garbage found in $($cfg.media_path)."
     }
-    Log ""
-
-    $garbageNum = 0
 
     ForEach ($turd in $garbageList) {
-        $garbageNum++
-        Log "$($garbageNum). $turd"
+        Log "`t$($garbageList.indexOf($turd)+1). $turd"
         Try {
             Remove-Item $turd -Force -ErrorAction Stop
         }
