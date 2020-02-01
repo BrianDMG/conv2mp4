d02e620625bbaad0df318315b9bc8e7e3e5cdb67
BrianDMG <github@teague.io>
2020-01-31 21:15:23 -0500

    Added GitLab URL

--- files/func/PrintVersion.ps1
+++ files/func/PrintVersion.ps1
@@ -1,4 +1,3 @@
 Function PrintVersion {
-    Log "conv2mp4-$($prop.platform) $($prop.version) - $($prop.github_url)"
-    Log "$($prop.standard_divider)"
+    Log "conv2mp4-$($prop.platform) $($prop.version) - $($prop.github_url) / $($prop.gitlab_url)"
 }
