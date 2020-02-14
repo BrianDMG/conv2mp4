2238cc08ae3aedb89788293666f82e6321729b53
BrianDMG <github@teague.io>
2020-02-13 20:34:18 -0500

    Formatting

--- files/init/buildqueue.ps1
+++ files/init/buildqueue.ps1
@@ -6,14 +6,14 @@ If ($cfg.use_ignore_list) {
     $cfg.include_file_types += ", *.mp4"
     $fileList = Get-ChildItem "$((Get-Item -Path $cfg.media_path).FullName)" -Include ( $cfg.include_file_types -split ',' ).trim() -Exclude $(Get-Content $prop.ignore_path) -Recurse |
         ForEach-Object {$counter = 1} {
-            Write-Progress -Activity "`rFound $($counter) file(s) so far..." -Status "Processing..." -CurrentOperation "$($_.FullName)"
+            Write-Progress -Activity "Found $($counter) file(s) so far..." -Status "Processing..." -CurrentOperation "$($_.FullName)"
             $counter++
         }
 }
 Else {
     $fileList = Get-ChildItem "$((Get-Item -Path $cfg.media_path).FullName)" -Include ( $cfg.include_file_types -split ',' ).trim() -Recurse |
         ForEach-Object {$counter = 1} {
-            Write-Progress -Activity "`rFound $($counter) file(s) so far..." -Status "Processing..." -CurrentOperation "$($_.FullName)"
+            Write-Progress -Activity "Found $($counter) file(s) so far..." -Status "Processing..." -CurrentOperation "$($_.FullName)"
             $counter++
         }
 }
