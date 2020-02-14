8536765f4f4ba5a9a8c9d42854dc1aa8aaaa91c7
BrianDMG <github@teague.io>
2020-02-13 21:23:41 -0500

    Added missing object add

--- files/init/buildqueue.ps1
+++ files/init/buildqueue.ps1
@@ -7,6 +7,7 @@ If ($cfg.use_ignore_list) {
     $fileList = Get-ChildItem "$((Get-Item -Path $cfg.media_path).FullName)" -Include ( $cfg.include_file_types -split ',' ).trim() -Exclude $(Get-Content $prop.ignore_path) -Recurse |
         ForEach-Object {$counter = 1} {
             Write-Progress -Activity "Found $($counter) file(s) so far..." -Status "Processing..." -CurrentOperation "$($_.FullName)"
+            $_
             $counter++
         }
 }
@@ -14,6 +15,7 @@ Else {
     $fileList = Get-ChildItem "$((Get-Item -Path $cfg.media_path).FullName)" -Include ( $cfg.include_file_types -split ',' ).trim() -Recurse |
         ForEach-Object {$counter = 1} {
             Write-Progress -Activity "Found $($counter) file(s) so far..." -Status "Processing..." -CurrentOperation "$($_.FullName)"
+            $_
             $counter++
         }
 }
