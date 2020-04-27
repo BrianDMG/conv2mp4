ae7d402b8d02ed2a7058523bfc1d2fd000b735fa
Brian <BrianDMG@users.noreply.github.com>
2020-04-27 19:54:57 -0400

    v4.1.2 (#66)
    *Fixed minor issue where failover threshold percentage was not being dynamically generated, leading to possibly incorrect reporting in console and logging.
    Closes #64

--- files/func/PrintEncodeError.ps1
+++ files/func/PrintEncodeError.ps1
@@ -1,10 +1,10 @@
-# If new file is over 25% smaller than the original file, trigger encoding failure
+# If file size delta exceeds failover threshold, trigger encoding failure
 Function PrintEncodeError {
     $fileSizeDelta = [Math]::Round($targetFileCompare.length - $sourceFileCompare.length)/1MB
     $fileSizeDelta = [Math]::Round($fileSizeDelta, 2)
     Try {
         Remove-Item -LiteralPath $targetFile -Force -ErrorAction Stop
-        Log "$($time.Invoke()) EXCEPTION: New file is over 25% smaller ($($fileSizeDelta)MB)."
+        Log "$($time.Invoke()) EXCEPTION: New file is over $($cfg.failover_threshold -replace '[.]','')% smaller ($($fileSizeDelta)MB)."
         Log "$($time.Invoke()) $targetFileRenamed deleted."
         Log "$($time.Invoke()) FAILOVER: Re-encoding $sourceFile with Handbrake."
     }
